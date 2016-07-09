68b6009108045fd4f0d5de8cd308a5a2384aa52a
Alex Costello <costello@uoregon.edu>
2016-07-09 16:41:44 -0700

    fuck site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,15 +1,17 @@
 node ip-10-0-4-244 {
+	include sshd
+
 	cron { "puppet update":
 		command => "cd /etc/puppet" && git pull -q origin master",
-		user => "root",
+		user => root,
 		minute => "*/5",
 	}
 }
 
-node ip-10-0-4-244 {
-
+node ip-10-0-4-164 {
+	include sshd
 }
 
 node ip-10-0-4-188{
-
+	include sshd
 }
