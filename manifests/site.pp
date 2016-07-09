2733907c43f3d9f011c95d8b8f7fd808a1755718
Alex Costello <costello@uoregon.edu>
2016-07-09 16:30:04 -0700

    actually fixed site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,22 +1,15 @@
-node ip-10-0-4-188{
-{	
-    command => "cd /etc/puppet && git pull -q origin master",
-        user    => root,
-	    minute  => "*/5",
-	    }
-}
-node ip-10-0-4-164{
-{
-    command => "cd /etc/puppet && git pull -q origin master",
-        user    => root,
-	    minute  => "*/5",
-	    }
+node ip-10-0-4-244 {
+	cron { "puppet update":
+		command => "cd /etc/puppet" && git pull -q origin master",
+		user => "root",
+		minute => "*/5",
+	}
 }
 
-node ip-10-0-4-244{
-{
-    command => "cd /etc/puppet && git pull -q origin master",
-        user    => root,
-	    minute  => "*/5",
-	    }
+node ip-10-0-4-244 {
+
+}
+
+node ip-10-0-4-188{
+
 }
