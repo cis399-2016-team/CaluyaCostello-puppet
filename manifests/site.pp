9c7e1395131db062d802f343092fb2c734db4e9b
Alex Costello <costello@uoregon.edu>
2016-07-09 19:04:56 -0700

    found syntax error in site.pp!

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,7 +2,7 @@ node ip-10-0-4-244 {
 	include sshd
 
 	cron { "puppet update":
-		command => "cd /etc/puppet" && git pull -q origin master",
+		command => "cd /etc/puppet && git pull -q origin master",
 		user => root,
 		minute => "*/5",
 	}
