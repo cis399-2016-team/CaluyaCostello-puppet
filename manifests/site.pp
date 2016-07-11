9a23f8e4c0489141fcf5448e1b775dc41e64d2ee
Alex Costello <costello@uoregon.edu>
2016-07-11 15:54:16 -0700

    added include sshd for puppet master

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,11 @@
 node 'ip-10-0-4-244' {
-
+	include sshd
 	cron { "puppet update":
 		command => "cd /etc/puppet && git pull -q origin master",
 		user => root,
 		minute => "*/5",
 	}
+
 }
 
 node 'ip-10-0-4-164' {
