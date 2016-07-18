57b6590fe912241c838cc783a54545a13c74180e
Alex Costello <costello@uoregon.edu>
2016-07-17 17:46:18 -0700

    added foo.html file and requirement for foo.html in init.pp

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -8,10 +8,19 @@ class apache {
 		group => root,
 		require => Package["apache2"]
 	}
+	file { '/var/www/html/foo.html':
+		source => 'pupper:///modules/apache/foo.html',
+		mode => 644,
+		owner => root,
+		group => root,
+		require => Package['apache2'],
+		}
 	service { "apache2":
 		enable => true,
 		ensure => running,
-		require => [Package["apache2"],File["/etc/apache2/apache2.conf"],],
+		require => [Package["apache2"],
+				File["/etc/apache2/apache2.conf"],
+				File['var/www/html/foo.html']],
 		subscribe => [File["/etc/apache2/apache2.conf"],]
 	}
 }
