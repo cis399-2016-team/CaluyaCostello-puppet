a80d5e30fe9a29245a776738272b1663671f3af1
Elijah Caluya <root@ip-10-0-4-244.us-west-2.compute.internal>
2016-07-25 22:31:09 +0000

    fixed typo in init.pp

--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -149,7 +149,7 @@ class users {
 		home => "/home/jstuemke",
 		managehome => true,
 	}
-	ssh_authorized_key{"jstuemke-key-pair-oregon",
+	ssh_authorized_key{"jstuemke-key-pair-oregon":
 		user => "jstuemke",
 		type => "ssh-rsa",
 		key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCPnelozzko4JcJxfgbm0Ib7h8C0dDpV5NvuKKuzVabFRSlc8MTXUz2aLAaiZzqMQQFdnxOLz0xJrQIpQIGRFiTvBfP1g5fCcHTRcjumv8e5qL+ARGbYgoCz+CMTsF71ZT4Y5U0nJaBuzgKiCNtXjG2Hma/pMT7dEWeUZiOymHihVzUkMa8kAiLkzRJB94ZCv8QeYyO7+cJsjbce4FwBQg+r9+Rwoj11eVj4fOvA1eOhp2d+S31E4GaaEfYrM3lRKQZQLLdygKxiHA3geHMJAJLQaiF3scfm/5NofTS6OxRsoC3q54Q6JXH104okrPYBt/EMRMEY3bO8FI364+LDU4J",
