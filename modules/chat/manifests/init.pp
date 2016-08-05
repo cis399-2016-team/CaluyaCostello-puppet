7787c9a3b3bdb35853a702d2e90a8fb8935ab409
Elijah Caluya <root@ip-10-0-4-244.us-west-2.compute.internal>
2016-08-05 23:05:06 +0000

    added chat module

---
+++ modules/chat/manifests/init.pp
@@ -0,0 +1,26 @@
+class chat {
+	
+	package {
+		"chat" :ensure => installed;
+	}
+
+	file {
+		"/etc/chat/chat.cfg":
+			source => ["puppet:///modules/chat/chat.cfg",],
+			mode => 444,
+			owner => root,
+			group => root,
+			require => Package["chat"],
+	}
+
+	service {
+		"chat":
+			enable => true,
+			ensure => running,
+			hasstatus => true,
+			hasrestart => true,
+			require => [Package["chat"],File["/etc/chat/chat.cfg"]],
+			subscribe => File["/etc/chat/chat.cfg"]
+	}
+
+}
